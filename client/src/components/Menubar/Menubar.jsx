da674fee370a1001f0480222993ef07c419de1b4
Ilya Kukharchuk <bartash12345@gmail.com>
2025-05-14 10:36:07 +0300

    add styles and architecture to category page

--- client/src/components/Menubar/Menubar.jsx
+++ client/src/components/Menubar/Menubar.jsx
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { assets } from "../../assets/assets";
-import "../../css/components/Menubar/menubar.css";
+import "../../css/components/menubar.css";
 
 export default function Menubar() {
+  const location = useLocation();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
       <a className="navbar-brand" href="#">
@@ -22,27 +24,58 @@ export default function Menubar() {
       <div className="collapse navbar-collapse p-2" id="navbarNav">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link to="/dashboard" className="nav-link active">
+            <Link
+              to="/dashboard"
+              className={
+                location.pathname === "/dashboard"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
               Dashboard
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/explore" className="nav-link">
+            <Link
+              to="/explore"
+              className={
+                location.pathname === "/explore"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
               Explore
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/items" className="nav-link">
+            <Link
+              to="/items"
+              className={
+                location.pathname === "/items" ? "nav-link active" : "nav-link"
+              }
+            >
               Manage items
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/category" className="nav-link">
+            <Link
+              to="/category"
+              className={
+                location.pathname === "/category"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
               Manage categories
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/users" className="nav-link">
+            <Link
+              to="/users"
+              className={
+                location.pathname === "/users" ? "nav-link active" : "nav-link"
+              }
+            >
               Manage users
             </Link>
           </li>
